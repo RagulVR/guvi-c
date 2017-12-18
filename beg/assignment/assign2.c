650346189413fcb2f79e38c347b28ce4d8436d30
RagulVR <34641072+RagulVR@users.noreply.github.com>
2017-12-18 14:51:31 +0530

    Create assign2.c

---
+++ beg/assignment/assign2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a,b;
+int c,e;
+float d;
+a = "Sachin Ramesh Tendulkar";
+b = "Little Master";
+c = 18426;
+f = 44.83;
+e = 463;
+printf("%c",a);
+printf("%c",b);
+printf("%d",c);
+printf("%f",f);
+printf("%d",e);
+}
