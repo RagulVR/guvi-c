ef1966540b2c4ae58ed3fd6d076734f01e3c73a2
RagulVR <34641072+RagulVR@users.noreply.github.com>
2017-12-18 15:09:48 +0530

    Create assign2.c

---
+++ beg/assignment/assign2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char a[]="Sachin Ramesh Tendulkar";
+char b[]= "Little Master";
+int c=18426;
+float d=44.83;
+int f= 463;
+printf("%s \n %s \n %d \n %.2f \n %d",a,b,c,d,f);
+}
