2834b02fe5a5f68670e563efe94df94510a4b95a
RagulVR <34641072+RagulVR@users.noreply.github.com>
2017-12-19 10:05:49 +0530

    Create positive,negative or 0.c

---
+++ Code katta/beg/set/positive,negative or 0.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main(void)
+{
+    int a;
+    printf("Enter the number");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("The number is positive");
+    }
+    else if(a<0)
+    {
+        printf("The number is negative");
+    }
+    else
+    {
+        printf("The number is zero");
+    }
+    return(0);
+}
